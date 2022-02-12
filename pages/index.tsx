045c374f87cf723ba78dfe33f7cc52e190078a50
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-12 23:57:01 +0800

    feat: change design

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,14 +22,14 @@ const Landing: NextPage = () => {
         className=" text-white "
         style={{
           backgroundColor: "#f7b42c",
-          backgroundImage: `linear-gradient(315deg, #f7b42c 0%, #fc575e 74%)`,
+          backgroundImage: `linear-gradient(95deg, #FC7619 0%, #B0581A 99%)`,
         }}
       >
         <div className="container-fluid p-lg-5 p-2">
           <div className="row flex-lg-row-reverse row-cols-lg-2 row-cols-1 justify-content-center align-items-center">
             <div className="col pt-lg-0 pt-5">
               <Image
-                src="/assets/hero_land.png"
+                src="/assets/hero.svg"
                 alt="Confession Board"
                 width={600}
                 height={500}
@@ -37,30 +37,30 @@ const Landing: NextPage = () => {
               />
             </div>
             <div className="col pe-3">
-              <h1 className="fw-bold text-center text-lg-start">
+              <h1 id="big-text" className="fw-bold text-center text-lg-start py-4">
                 Confession Board
               </h1>
-              <hr />
+              
               <p className="text-center fs-4 text-lg-start">
                 Confession Board is a platform for{" "}
-                <span className="text-dark fw-bold">sharing</span> your
+                <span className=" fw-bold" style={{color:'#FCDB00'}}>sharing</span> your
                 confessions
-                <span className="text-dark fw-bold"> anonymously</span>. Share
+                <span className=" fw-bold" style={{color:'#FCDB00'}}> anonymously</span>. Share
                 your deepest secrets anonymously and get anonymous feedback from
                 other users. Clear your doubts and face the secrets of yourself.
               </p>
-              <hr />
+              
               <p className="fw-normal text-center text-lg-start">
                 "I am a person who is not afraid to speak my mind. I am a person
                 who is not afraid to be myself." -{" "}
                 <span className="fst-italic"> Anonymous </span>
               </p>
               <div className="d-flex flex-column flex-lg-row">
-                <a href="/create" className="btn btn-primary m-2">
+                <a href="/create" className="btn btn-primary m-2 px-5 py-3">
                   {" "}
                   Create Confession{" "}
                 </a>
-                <a href="/home" className="btn btn-outline-dark m-2">
+                <a href="/home" className="btn btn-outline-dark m-2 px-5 py-3">
                   {" "}
                   Read Confessions{" "}
                 </a>

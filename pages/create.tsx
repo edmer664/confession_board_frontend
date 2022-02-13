4c543c0159cbc81f1e82891faff46cae461662e3
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-13 18:43:01 +0800

    feat: UX fixes

--- pages/create.tsx
+++ pages/create.tsx
@@ -25,10 +25,10 @@ export default function create() {
                     <div className="container">
                         <form action='https://confession-board.herokuapp.com/api/make-post/' method='POST'>
                             <div className="form-group">
-                                <label htmlFor="title">Title</label>
-                                <input type="text" className="form-control" id="title" name='title' placeholder="Title" />
+                                <label htmlFor="title">Dedicated To</label>
+                                <input type="text" className="form-control" id="title" name='title' placeholder="Juan Dela Cruz" />
                                 <label htmlFor="content">Content</label>
-                                <textarea className="form-control" id="content" name="body" rows={3} ></textarea>
+                                <textarea className="form-control" id="content" name="body" placeholder="Type your confessions here..." rows={3} ></textarea>
                                 <label htmlFor="author" >Author (optional)</label>
                                 <input type="text" name="author"  className="form-control" id="author" placeholder="Author" />
                                 <button type="submit" className="btn btn-primary my-3">Submit</button>

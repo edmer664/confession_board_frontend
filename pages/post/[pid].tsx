c66fbd99877e685e3e13d531755f9ff921492f29
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-13 15:44:57 +0800

    feat: new page

---
+++ pages/post/[pid].tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+import { useRouter } from "next/router"
+import Head from "next/head"
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+
+
+export default function Posts() {
+    const router = useRouter()
+    const { pid } = router.query
+
+
+  return (
+    <>
+        <Head>
+            <title>Confession</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <Navbar />
+            
+        <Footer />
+    </>
+  )
+}

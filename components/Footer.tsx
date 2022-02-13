e667fb885df93cc221f56a3072205cd6a7e289f6
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-13 15:39:51 +0800

    fix: style changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   return (
     <footer
       id="sticky-footer"
-      className="flex-shrink-0 py-4 bg-dark text-white-50"
+      className="flex-shrink-0 py-4 bg-dark text-white-50 "
     >
       <div className="container text-center d-flex flex-column">
         <small>
